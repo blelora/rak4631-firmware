c23ce01709f957bcb597da5793c560b2216ffdf3
Kent Williams <k3nt00@gmail.com>
2022-01-18 15:47:01 -0800

    add debug printing

--- include/main.h
+++ include/main.h
@@ -1,6 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+// Debug output, 0 to disable, 1 to enable
+#define MY_DEBUG 1
+
+#if MY_DEBUG > 0
+#define MYLOG(tag, ...)           \
+    do                            \
+    {                             \
+        if (tag)                  \
+            PRINTF("[%s] ", tag); \
+        PRINTF(__VA_ARGS__);      \
+        PRINTF("\n\r");           \
+    } while (0)
+#else
+#define MYLOG(...)
+#endif
+
 #include <Arduino.h>
 #include <nrf_nvic.h>
 
--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,36 @@ void setup()
     // Initialize the built in Green LED
     pinMode(LED_GREEN, OUTPUT);
     digitalWrite(LED_GREEN, LOW);
+
+#if MY_DEBUG > 0
+    // Initialize Serial for debug output
+    Serial.begin(115200);
+
+    time_t serial_timeout = millis();
+    // On nRF52840 the USB serial is not available immediately
+    while (!Serial)
+    {
+        if ((millis() - serial_timeout) < 5000)
+        {
+            delay(100);
+            digitalWrite(LED_GREEN, !digitalRead(LED_GREEN));
+        }
+        else
+        {
+            break;
+        }
+    }
+#endif
+
+    digitalWrite(LED_GREEN, HIGH);
+
+    MYLOG("APP", "=====================================");
+    MYLOG("APP", "RAK4631 Firmware Starting");
+    MYLOG("APP", "=====================================");
 }
 
 void loop()
 {
-    // Main loop running indicator
-    digitalToggle(LED_GREEN);
-    delay(200);
+    MYLOG("APP", "============= Main Loop =============");
+    delay(1000);
 }
