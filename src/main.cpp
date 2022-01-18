8f3f1feaa50932ce1ec9644fab8e8b745ab712f9
Kent Williams <k3nt00@gmail.com>
2022-01-18 13:58:34 -0800

    main loop led toggle

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include "main.h"
 
+#define LED_GREEN PIN_LED1
+#define LED_BLUE PIN_LED2
+
 void setup()
 {
+    // Initialize the built in Green LED
+    pinMode(LED_GREEN, OUTPUT);
+    digitalWrite(LED_GREEN, LOW);
 }
 
 void loop()
 {
+    // Main loop running indicator
+    digitalToggle(LED_GREEN);
+    delay(200);
 }
