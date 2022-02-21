dd26ab720ed867e2578fda993dbdc29be450a447
Kent Williams <k3nt00@gmail.com>
2022-02-21 15:49:51 -0800

    LoRaWAN Basics (#3)
    * lorawan join and tx working
    
    * downlink working
    
    * add vscode extension to gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,12 @@ SoftwareTimer g_task_wakeup_timer;
 // Flag for the event type
 volatile uint16_t g_task_event_type = NO_EVENT;
 
+/** Flag showing if TX cycle is ongoing */
+bool lora_busy = false;
+
+/** Set the device name, max length is 10 characters */
+char lora_packet[10] = "HELLO";
+
 // Period Wakeup for Statuses
 void periodic_wakeup(TimerHandle_t unused)
 {
@@ -60,8 +66,10 @@ void setup()
     // Init BLE
     init_ble();
 
+    init_lorawan();
+
     // LoRa is setup, start the timer that will wakeup the loop frequently
-    g_task_wakeup_timer.begin(500, periodic_wakeup);
+    g_task_wakeup_timer.begin(5000, periodic_wakeup);
     g_task_wakeup_timer.start();
 
     DEBUG_LOG("APP", "Setup Complete");
@@ -79,20 +87,46 @@ void loop()
         digitalWrite(LED_BUILTIN, HIGH);
         while (g_task_event_type != NO_EVENT)
         {
+            // Handle LoRa data events
+			lora_data_handler();
             // Timer triggered event, check serial
             if ((g_task_event_type & STATUS) == STATUS)
             {
                 g_task_event_type &= N_STATUS;
                 int temp;
                 while (Serial.available() > 0)
-				{
+                {
                     temp = Serial.read();
-					Serial.printf("%c", temp);
+                    Serial.printf("%c", temp);
                     ble_uart.printf("%c", temp);
-					delay(5);
-				}
+                    delay(5);
+                }
                 // DEBUG_LOG("SERIAL", "Received Command");
                 // DEBUG_LOG("APP", "Timer wakeup");
+                if (lora_busy)
+                {
+                    DEBUG_LOG("APP", "LoRaWAN TX cycle not finished, skip this event");
+                }
+                else
+                {
+                    lmh_error_status result;
+                    result = send_lora_packet((uint8_t *)&lora_packet, 10);
+
+                    switch (result)
+                    {
+                    case LMH_SUCCESS:
+                        DEBUG_LOG("APP", "Packet enqueued");
+                        /// \todo set a flag that TX cycle is running
+                        lora_busy = true;
+                        break;
+                    case LMH_BUSY:
+                        DEBUG_LOG("APP", "LoRa transceiver is busy");
+                        break;
+                    case LMH_ERROR:
+                        DEBUG_LOG("APP", "Packet error, too big to send with current DR");
+                        break;
+                    }
+                }
             }
             // BLE UART data handling
             if ((g_task_event_type & BLE_DATA) == BLE_DATA)
@@ -112,14 +146,70 @@ void loop()
         }
 
         // reset main loop timer
-		g_task_wakeup_timer.reset();
+        g_task_wakeup_timer.reset();
 
         Serial.flush();
         g_task_event_type = 0;
         // Go back to sleep
-        xSemaphoreTake(g_task_sem, 500);
+        xSemaphoreTake(g_task_sem, 10);
         // Switch off blue LED to show we go to sleep
         digitalWrite(LED_BUILTIN, LOW);
         delay(10);
     }
+}
+
+/**
+ * @brief Handle received LoRa Data
+ * 
+ */
+void lora_data_handler(void)
+{
+	// LoRa Join finished handling
+	if ((g_task_event_type & LORA_JOIN_FIN) == LORA_JOIN_FIN)
+	{
+		g_task_event_type &= N_LORA_JOIN_FIN;
+		if (g_join_result)
+		{
+			DEBUG_LOG("APP", "Successfully joined network");
+		}
+		else
+		{
+			DEBUG_LOG("APP", "Join network failed");
+			/// \todo here join could be restarted.
+			// lmh_join();
+		}
+	}
+
+	// LoRa data handling
+	if ((g_task_event_type & LORA_DATA) == LORA_DATA)
+	{
+		/**************************************************************/
+		/**************************************************************/
+		/// \todo LoRa data arrived
+		/// \todo parse them here
+		/**************************************************************/
+		/**************************************************************/
+		g_task_event_type &= N_LORA_DATA;
+		DEBUG_LOG("APP", "Received package over LoRa");
+		char log_buff[g_rx_data_len * 3] = {0};
+		uint8_t log_idx = 0;
+		for (int idx = 0; idx < g_rx_data_len; idx++)
+		{
+			sprintf(&log_buff[log_idx], "%02X ", g_rx_lora_data[idx]);
+			log_idx += 3;
+		}
+		lora_busy = false;
+		DEBUG_LOG("APP", "%s", log_buff);
+	}
+
+	// LoRa TX finished handling
+	if ((g_task_event_type & LORA_TX_FIN) == LORA_TX_FIN)
+	{
+		g_task_event_type &= N_LORA_TX_FIN;
+
+		DEBUG_LOG("APP", "LPWAN TX cycle %s", g_rx_fin_result ? "finished ACK" : "failed NAK");
+
+		/// \todo reset flag that TX cycle is running
+		lora_busy = false;
+	}
 }
