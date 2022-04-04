50eafb9b05b2c052de9bce2b7f6ed28fe1bb52e5
Kent Williams <k3nt00@gmail.com>
2022-04-04 14:21:00 -0700

    separate out lorawan settings structs

--- include/main.h
+++ include/main.h
@@ -58,12 +58,12 @@ extern atcmd_t g_user_at_cmd_list[] __attribute__((weak));
 #define N_LORA_TX 0b1111111101111111 
 
 void periodic_wakeup(TimerHandle_t unused);
+void lora_tx_wakeup(TimerHandle_t unused);
 extern SemaphoreHandle_t g_task_sem;
 extern volatile uint16_t g_task_event_type;
 extern SoftwareTimer g_task_wakeup_timer;
 extern SoftwareTimer g_task_lora_tx_wakeup_timer;
 
-// #define LORAWAN_CREDENTIALS_MARKER 0x45
 struct s_lorawan_credentials
 {
 	// OTAA Device EUI MSB
@@ -74,20 +74,12 @@ struct s_lorawan_credentials
 	uint8_t node_app_key[16] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 };
 
-#define LORAWAN_SETTINGS_MARKER 0x56
 struct s_lorawan_settings
 {
-	uint8_t valid_mark_1 = 0xAA;				// Just a marker for the Flash
-	uint8_t valid_mark_2 = LORAWAN_SETTINGS_MARKER; // Just a marker for the Flash
-
-	// Flag if node joins automatically after reboot
-	bool auto_join = false;
 	// Send repeat time in milliseconds: 2 * 60 * 1000 => 2 minutes
 	uint32_t send_repeat_time = 10000;
 	// Flag for ADR on or off
 	bool adr_enabled = false;
-	// Flag for public or private network
-	bool public_network = true;
 	// Flag to enable duty cycle (validity depends on Region)
 	bool duty_cycle_enabled = false;
 	// Number of join retries
@@ -96,22 +88,30 @@ struct s_lorawan_settings
 	uint8_t tx_power = 0;
 	// Data rate 0 .. 15 (validity depends on Region)
 	uint8_t data_rate = 3;
-	// LoRaWAN class 0: A, 2: C, 1: B is not supported
-	uint8_t lora_class = 0;
 	// Subband channel selection 1 .. 9
 	uint8_t subband_channels = 2;
 	// Data port to send data
 	uint8_t app_port = 2;
 	// Flag to enable confirmed messages
 	lmh_confirm confirmed_msg_enabled = LMH_UNCONFIRMED_MSG;
-	// Command from BLE to reset device
-	bool resetRequest = true;
 	// LoRa region
 	uint8_t lora_region = LORAMAC_REGION_US915;
+};
+struct s_lorawan_static_settings
+{
+	// Flag if node joins automatically after reboot
+	bool auto_join = false;
+	// Flag for public or private network
+	bool public_network = true;
+	// LoRaWAN class 0: A, 2: C, 1: B is not supported
+	uint8_t lora_class = 0;
+	// Command from BLE to reset device
+	bool resetRequest = true;
 };
 
 extern s_lorawan_credentials g_lorawan_credentials;
 extern s_lorawan_settings g_lorawan_settings;
+extern s_lorawan_static_settings g_lorawan_static_settings;
 extern uint8_t g_lorawan_credentials_status;
 extern uint8_t g_rx_lora_data[];
 extern uint8_t g_rx_data_len;
